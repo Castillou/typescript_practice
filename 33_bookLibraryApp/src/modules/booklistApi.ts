48731862c04b6e9283a09107bd7121dd856fa339
Sean <joajoa70584@gmail.com>
2024-12-30 14:37:12 +0900

    :art:test: Member, Book 샘플코드 수정

--- 33_bookLibraryApp/src/modules/booklistApi.ts
+++ 33_bookLibraryApp/src/modules/booklistApi.ts
@@ -1,9 +1,6 @@
 import Book, { ITBook, CookBook, ComicBook } from '../model/book';
-import { memberList } from './memberlistApi';
 
-const [member1, member2, member3] = memberList;
-
-const bookList: Array<Book> = [
+export const bookList: Array<Book> = [
 	new ITBook(100, '모던 자바스크립트', '이웅모', 'Javascript'),
 	new ITBook(101, '자바의 정석', '남궁성', 'Java'),
 	new ITBook(102, '열혈C언어', '윤성우', 'C'),
@@ -16,18 +13,6 @@ const bookList: Array<Book> = [
 	new ComicBook(302, '신의탑', '시우'),
 	new ComicBook(303, '귀멸의칼날', '악어'),
 ];
-
-bookList.forEach((book, idx) => {
-	if (idx < 2) {
-		book.owner = member1;
-	}
-	if (idx === 4 || idx === 5) {
-		book.owner = member2;
-	}
-	if (idx >= 9) {
-		book.owner = member3;
-	}
-});
 
 // memberApi
 export function getBookListAll(): Book[] {
--- 33_bookLibraryApp/src/modules/memberlistApi.ts
+++ 33_bookLibraryApp/src/modules/memberlistApi.ts
@@ -1,13 +1,20 @@
 import Member from '../model/member';
+import { bookList } from './booklistApi';
 
 // member sample
 const member1 = new Member(1, 'user1', '1234', 'Sean');
+member1.borrowBookList = [bookList[0], bookList[1], bookList[2]];
+member1.borrowBookList.forEach((book) => (book.owner = member1));
 
 const member2 = new Member(2, 'user2', '1234', 'Alice');
+member2.borrowBookList = [bookList[4], bookList[5]];
+member2.borrowBookList.forEach((book) => (book.owner = member2));
 
 const member3 = new Member(3, 'user3', '1234', 'John');
+member3.borrowBookList = [bookList[9], bookList[10]];
+member3.borrowBookList.forEach((book) => (book.owner = member3));
 
-export const memberList: Array<Member> = [member1, member2, member3];
+const memberList: Array<Member> = [member1, member2, member3];
 
 // memberApi
 export function getMemberListAll(): Member[] {
