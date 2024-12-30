a66c2f8c4b36c4e28ff89797d60754bb1b5bb83d
Sean <joajoa70584@gmail.com>
2024-12-30 10:48:39 +0900

    :white_check_mark:test: ITBook 클래스 추가

--- 33_bookLibraryApp/src/model/book.ts
+++ 33_bookLibraryApp/src/model/book.ts
@@ -24,4 +24,21 @@ abstract class Book {
 	public abstract info(): string;
 }
 
+export class ITBook extends Book {
+	private language: string;
+
+	constructor(bno: number, title: string, writer: string, owner: Member | null, language: string) {
+		super(bno, title, writer, owner);
+		this.language = language;
+	}
+
+	public info(): string {
+		return `${this.bno} - ITBook / ${this.title} / ${this.writer} / ${this.language}`;
+	}
+
+	public getLanguage(): string {
+		return this.language;
+	}
+}
+
 export default Book;
